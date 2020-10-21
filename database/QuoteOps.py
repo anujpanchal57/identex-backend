bc4205d892d8acce2d919ad51e67c2d8ff031ce8
Anuj Panchal <anuj.panchal@exportify.in>
2020-10-21 15:43:26 +0530

    /quotes/get -- po_id added

--- database/QuoteOps.py
+++ database/QuoteOps.py
@@ -106,7 +106,7 @@ class Quote:
         try:
             self.__cursor.execute("""select s.company_name as supplier_company_name, s.supplier_id, qu.amount, qu.delivery_time, q.quote_validity,
                                     qu.charge_id, qu.quote_id, qu.confirmed, qu.charge_name, qu.quantity, qu.gst, qu.per_unit,
-                                    qu.logistics_included, q.payment_terms, q.remarks
+                                    qu.logistics_included, q.payment_terms, q.remarks, qu.po_id
                                     from suppliers as s
                                     join quotations as q
                                     on s.supplier_id = q.supplier_id
@@ -209,7 +209,7 @@ class Quote:
             if category.lower() == "cheapest":
                 self.__cursor.execute("""select s.company_name as supplier_company_name, s.supplier_id, qu.amount, qu.delivery_time, q.quote_validity,
                                         qu.quote_id, qu.charge_id, qu.confirmed, qu.charge_name, qu.quantity, qu.gst, qu.per_unit,
-                                        qu.logistics_included, q.payment_terms, q.remarks
+                                        qu.logistics_included, q.payment_terms, q.remarks, qu.po_id
                                         from suppliers as s
                                         join quotations as q
                                         on s.supplier_id = q.supplier_id
@@ -225,7 +225,7 @@ class Quote:
             elif category.lower() == "fastest":
                 self.__cursor.execute("""select s.company_name as supplier_company_name, s.supplier_id, qu.amount, qu.delivery_time, q.quote_validity,
                                         qu.quote_id, qu.charge_id, qu.confirmed, qu.charge_name, qu.quantity, qu.gst, qu.per_unit,
-                                        qu.logistics_included, q.payment_terms, q.remarks
+                                        qu.logistics_included, q.payment_terms, q.remarks, qu.po_id
                                         from suppliers as s
                                         join quotations as q
                                         on s.supplier_id = q.supplier_id
